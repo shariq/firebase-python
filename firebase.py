a9517def68ec25ec4ba3f007b24e7e23a6f9bbd9
hashme <shariqhashme@gmail.com>
2014-12-14 05:07:17 -0500

    made firebase.py PEP8 compliant

--- firebase.py
+++ firebase.py
@@ -8,32 +8,36 @@ import json
 import threading
 import socket
 
+
 class ClosableSSEClient(SSEClient):
+
     def __init__(self, *args, **kwargs):
         self.should_connect = True
         super(ClosableSSEClient, self).__init__(*args, **kwargs)
+
     def _connect(self):
         if self.should_connect:
             super(ClosableSSEClient, self)._connect()
         else:
             raise StopIteration()
+
     def close(self):
         self.should_connect = False
         self.retry = 0
-        # HACK: dig through the sseclient library to the requests library down to the underlying socket.
-        # then close that to raise an exception to get out of streaming. I should probably file an issue w/ the
-        # requests library to make this easier
         try:
             self.resp.raw._fp.fp._sock.shutdown(socket.SHUT_RDWR)
             self.resp.raw._fp.fp._sock.close()
         except AttributeError:
             pass
 
+
 class RemoteThread(threading.Thread):
+
     def __init__(self, URL, function):
         self.function = function
         self.URL = URL
         super(RemoteThread, self).__init__()
+
     def run(self):
         try:
             self.sse = ClosableSSEClient(self.URL)
@@ -46,9 +50,11 @@ class RemoteThread(threading.Thread):
             pass    # this can happen when we close the stream
         except KeyboardInterrupt:
             self.close()
+
     def close(self):
         if self.sse:
             self.sse.close()
+
 
 def firebaseURL(URL):
     if '.firebaseio.com' not in URL.lower():
@@ -57,9 +63,10 @@ def firebaseURL(URL):
         if '/' in URL:
             if '/' == URL[-1]:
                 URL = URL[:-1]
-            URL = 'https://'+URL.split('/')[0]+'.firebaseio.com/' + URL.split('/',1)[1] + '.json'
+            URL = 'https://' + \
+                URL.split('/')[0] + '.firebaseio.com/' + URL.split('/', 1)[1] + '.json'
         else:
-            URL = 'https://'+URL+'.firebaseio.com/.json'
+            URL = 'https://' + URL + '.firebaseio.com/.json'
         return URL
 
     if 'http://' in URL:
@@ -73,36 +80,51 @@ def firebaseURL(URL):
             URL = URL + '.json'
     return URL
 
+
 class subscriber:
+
     def __init__(self, URL, function):
         self.remote_thread = RemoteThread(firebaseURL(URL), function)
+
     def start(self):
         self.remote_thread.start()
+
     def stop(self):
         self.remote_thread.close()
         self.remote_thread.join()
+
     def wait(self):
         self.remote_thread.join()
 
+
+class FirebaseException(Exception):
+    pass
+
+
 def put(URL, msg):
     to_post = json.dumps(msg)
-    response = requests.put(firebaseURL(URL), data = to_post)
+    response = requests.put(firebaseURL(URL), data=to_post)
     if response.status_code != 200:
-        raise Exception(response.text)
+        raise FirebaseException(response.text)
+
 
 def patch(URL, msg):
     to_post = json.dumps(msg)
-    response = requests.patch(firebaseURL(URL), data = to_post)
+    response = requests.patch(firebaseURL(URL), data=to_post)
     if response.status_code != 200:
-        raise Exception(response.text)
+        raise FirebaseException(response.text)
 
 
-'''
-Yuck, I don't want to write documentation for this :p
-def push(URL, msg):
-    to_post = json.dumps(msg)
-    response = requests.post(firebaseURL(URL), data = to_post)
+def get(URL):
+    response = requests.get(firebaseURL(URL))
     if response.status_code != 200:
-        raise Exception(response.text)
-'''
+        raise FirebaseException(response.text)
+    return json.loads(response.text)
 
+
+# Yuck, I don't want to write documentation for this :p
+#def push(URL, msg):
+#    to_post = json.dumps(msg)
+#    response = requests.post(firebaseURL(URL), data=to_post)
+#    if response.status_code != 200:
+#        raise Exception(response.text)
